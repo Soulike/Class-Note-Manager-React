93e4e0376d3191c5f8e8373cf16fa3d2d43064d9
Soulike <316413310@qq.com>
2018-10-21 21:42:42 +0800

    完成文章列表

---
+++ src/Pages/NoteList/Components/NoteCard/View.js
@@ -0,0 +1,79 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import * as solidIcon from '@fortawesome/free-solid-svg-icons';
+import style from './NoteCard.module.scss';
+import {generateTimeStr, postAsync, requestPrefix} from '../../../../Static/Functions';
+import {View as Modal} from '../../../../Components/Modal';
+import {View as Alert} from '../../../../Components/Alert';
+
+class NoteCard extends Component
+{
+    onDeleteButtonClick = (e) =>
+    {
+        e.preventDefault();
+        const {name} = this.props;
+        let confirm = false;
+
+        Modal.show('删除确认', `你确实要删除笔记 ${name}？此操作不可逆！`, () =>
+        {
+            confirm = true;
+            if (confirm)
+            {
+                const {id} = this.props;
+                postAsync(requestPrefix('/deleteNote'), {id})
+                    .then(res =>
+                    {
+                        const {isSuccess, msg, data} = res;
+                        Alert.show(msg, isSuccess);
+                    })
+                    .catch(e =>
+                    {
+                        Alert.show('删除失败', false);
+                        console.log(e);
+                    });
+            }
+        });
+    };
+
+
+    render()
+    {
+        const {id, name, lastModified} = this.props;
+        return (
+            <div className={style.NoteCard}>
+                <div className={style.left}>
+                    <FontAwesomeIcon icon={solidIcon.faStickyNote} style={style.leftIcon}/>
+                </div>
+                <div className={style.right}>
+                    <div className={style.upper}>
+                        <div className={style.noteName}>{name}</div>
+                        <div className={style.buttonWrapper}>
+                            <Link onlyActiveOnIndex={false} to={`/NoteWriter?noteId=${id}`} target={'_blank'}>
+                                <button className={style.modifyButton} title={'修改笔记'}>
+                                    <FontAwesomeIcon icon={solidIcon.faPencilAlt} className={style.buttonIcon}/>
+                                </button>
+                            </Link>
+                            <button className={style.deleteButton} onClick={this.onDeleteButtonClick} title={'删除笔记'}>
+                                <FontAwesomeIcon icon={solidIcon.faTimes} className={style.buttonIcon}/>
+                            </button>
+                        </div>
+                    </div>
+                    <div className={style.lastModifiedDate}>
+                        最后修改：
+                        {generateTimeStr(lastModified)}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+NoteCard.propTypes = {
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    lastModified: PropTypes.number.isRequired
+};
+
+export default NoteCard;
---
+++ src/Pages/NoteList/View.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+import style from './NoteList.module.scss';
+import {View as NoteCard} from './Components/NoteCard';
+import {View as Title} from '../../Components/Title';
+import {getAsync, requestPrefix} from '../../Static/Functions';
+import {View as Alert} from '../../Components/Alert';
+
+class NoteList extends Component
+{
+    constructor()
+    {
+        super(...arguments);
+        this.state = {
+            noteList: []
+        };
+    }
+
+    componentDidMount()
+    {
+        getAsync(requestPrefix('/getNoteList'), false)
+            .then(res =>
+            {
+                const {isSuccess, msg, data} = res;
+                if (isSuccess)
+                {
+                    this.setState({noteList: data});
+                }
+                else
+                {
+                    Alert.show(msg, false);
+                }
+            })
+            .catch(e =>
+            {
+                Alert.show('获取笔记列表失败', false);
+                console.log(e);
+            });
+    }
+
+    render()
+    {
+        const {noteList} = this.state;
+        return (
+            <div className={style.NoteList}>
+                <Title text={'笔记列表'}/>
+                <div className={style.listArea}>
+                    {
+                        noteList.length === 0 ? <h2 style={{textAlign: 'center'}}>你没有笔记的样子，快去写个新笔记吧！</h2> :
+                            noteList.map(note =>
+                            {
+                                return (
+                                    <Link onlyActiveOnIndex={false}
+                                          to={`/NoteReader?id=${note.id}`}
+                                          target={'_blank'}
+                                          key={note.id}>
+                                        <NoteCard {...note}/>
+                                    </Link>
+                                );
+                            })
+                    }
+                </div>
+            </div>
+        );
+    }
+
+}
+
+
+export default NoteList;
